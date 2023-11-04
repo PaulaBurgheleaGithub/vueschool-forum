305731531a862af42db54b4a34d3651c23e0fe78
Paula Burghelea <paulaburghelea.github@gmail.com>
2023-11-04 21:23:00 +0000

    PageHome and methods to render user and articles

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome Paula!"/>
+  <PageHome />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import PageHome from './components/PageHome.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PageHome
   }
 }
 </script>
---
+++ src/components/PageHome.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<h1>Hello from Home Page!</h1>
+	</div>
+	<article v-for="thread in threads" :key="thread.id">
+		<h3>{{ thread.title }}</h3>
+		<div v-for="postId in thread.posts" :key="postId">
+			<p>{{ userById(postById(postId).userId).name }}</p>
+			<p>{{ postById(postId).text }}</p>
+		</div>
+	</article>
+</template>
+
+<script>
+import sourceData from '@/data.json';
+
+export default {
+	data() {
+		return {
+			threads: sourceData.threads,
+			posts: sourceData.posts,
+			users: sourceData.users
+		}
+	},
+	methods: {
+		postById(postId) {
+			return this.posts.find(p => p.id === postId)
+		},
+		userById(userId) {
+			return this.users.find(p => p.id === userId)
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
